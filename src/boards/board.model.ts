633f269a3fbced44460e2efd5c315da55d168966
KangJuHyeon <alinos@naver.com>
2022-03-11 16:03:11 +0900

    게시물 생성하기 (Service)

--- src/boards/board.model.ts
+++ src/boards/board.model.ts
@@ -9,3 +9,7 @@ export enum BoardStatus {
   PUBLIC = 'PUBLIC',
   PRIVATE = 'PRIVATE',
 }
+
+// 타입을 정의해주는 이유는?
+// 타입을 정의해주므로서 원하는 타입과 다른 코드를 사용할 시 에러가 남
+// 코드를 읽는 입장에서 더 코드를 쉽게 이해하고 읽을 수 있다.
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 // 이 NestJS는 이것을 이용해서 다른 컴포넌트에서 이 서비스를 사용 할 수 있게(injectable)만들어줍니다.
 @Injectable()
 export class BoardsService {
@@ -8,5 +8,17 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
