5601e965b30c0ad82759b719e541c1e64425fded
KangJuHyeon <alinos@naver.com>
2022-03-11 16:33:08 +0900

    게시물 생성을 위한 DTO

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 // 이 NestJS는 이것을 이용해서 다른 컴포넌트에서 이 서비스를 사용 할 수 있게(injectable)만들어줍니다.
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,10 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    //   const title = createBoardDto.title; 이 코드보단 아래 코드가 더 best
+    const { title, description } = createBoardDto;
+
     const board: Board = {
       id: uuid(),
       title,
